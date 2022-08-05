3f82a4cfd34db258d18996aee0ced2ce292e54c2
FilipKokowski <filipkokowski999@gmail.com>
2022-08-05 13:59:40 +0200

    part of wiper-app cli functionality (continuously wiping, wiping once)

--- src/wiper_control_app/main.cpp
+++ src/wiper_control_app/main.cpp
@@ -1,7 +1,135 @@
 #include <iostream>
+#include <string>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main()
-{
-    std::cout << "Hello World\n";
-    return 0;
+
+void drawWipers(int state, bool sprinklersOn){
+    int lenghtOfWipers = 6;
+    switch(state){
+        case 0:
+            if(!sprinklersOn){
+                for(short spacing = 0; spacing < lenghtOfWipers - 1; spacing++) std::cout << "\n";
+            }
+            std::cout << "<";
+            for(short count = 0; count <= lenghtOfWipers * 2 + 1; count++){
+                std::cout << "--";
+                
+                if(count == lenghtOfWipers) std::cout << " <";
+            }
+
+            std::cout << std::endl;
+            break;
+        case 1:
+            for(short count = 0; count < lenghtOfWipers; count++){
+                for(short spacing = 0; spacing < count; spacing++) std::cout << " ";
+                std::cout << "       \\";
+                std::cout << "          \\" << std::endl; 
+            }
+            break;
+        case 2:
+            for(short count = 0; count < lenghtOfWipers; count++) std::cout << "             |          |" << std::endl; 
+
+            break;
+        case 3:
+            for(short count = 0; count < lenghtOfWipers; count++){
+                for(short spacing = lenghtOfWipers * 2; spacing > count; spacing--) std::cout << " ";
+                std::cout << "       /";
+                std::cout << "          /" << std::endl; 
+            }
+            break;
+        case 4:
+            for(short spacing = 0; spacing < lenghtOfWipers - 1; spacing++) std::cout << "\n";
+            std::cout << "             ";
+            for(short count = 0; count <= lenghtOfWipers * 2 + 1; count++){
+
+                std::cout << "--";
+                
+                if(count == lenghtOfWipers) std::cout << "> ";
+            }
+
+            std::cout << ">" << std::endl;
+            break;
+        
+        default:
+            std::cout << "Choose option from the range (0-3)\n";
+    }
+}
+
+bool stopWiping;
+
+void wipe(short mode){
+    short wiperState = 0;
+    if(mode == 2){
+        int count = 0;
+        for(;;){
+            if(count == 6) break;
+            std::cout<< u8"\033[2J\033[1;1H";
+            for(short sprinklers = 0; sprinklers <= count; sprinklers++){
+                if(sprinklers == 0) std::cout << "     T";
+                else std::cout << "     |\n";
+            }
+            drawWipers(0, true);
+            count++;
+            usleep(250000);
+        }
+    }
+    for(;;){
+        std::cout<< u8"\033[2J\033[1;1H";
+        drawWipers(wiperState, false);
+        wiperState++;
+        usleep(500000);
+        if(wiperState > 4){
+            for(short backwardRun = 3; backwardRun > 0; backwardRun--){
+                std::cout<< u8"\033[2J\033[1;1H";
+                drawWipers(backwardRun, false);
+                usleep(500000);
+            }
+            wiperState = 0;
+            if(mode == 1 || stopWiping) return;
+        } 
+    }
+}
+
+int main(){
+    int mode = 0;
+    bool turnedOn = false;
+
+    for(;;){
+        if(!turnedOn) drawWipers(0, false);
+        if(turnedOn){
+            system("clear");
+            switch(mode){
+                //Wipe continuously 
+                case 0:
+                    wipe(0);
+                    break;
+
+                //Wipe once
+                case 1:
+                    wipe(1);
+                    break;
+
+                //Wipe once with fluid
+                case 2:
+                    wipe(2);
+                    break;
+                
+                //Stop wiping
+                case 3:
+                    stopWiping = true;
+                    break;
+                default:
+
+                break;
+
+            }
+            turnedOn = false;
+        }
+        else{
+            std::cout << "\n\n";
+            std::cin >> mode;
+            turnedOn = true;
+        }
+    }
 }
