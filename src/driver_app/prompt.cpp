c383e291a575e51e2fa512e83dd207aa62b376d4
akinhet <piti200406@gmail.com>
2022-08-11 11:01:34 +0200

    PR5-121 Added line editing and history to the cli

--- src/driver_app/prompt.cpp
+++ src/driver_app/prompt.cpp
@@ -1,13 +1,17 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <string>
-#include "prompt.h"
 #include "mqtt/client.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "prompt.h"
 
 const std::string ADDRESS {"tcp://localhost:1883"};
 const int QOS = 1;
 mqtt::connect_options connOpts;
-mqtt::client cli(ADDRESS, "data_publish_example");
+mqtt::client cli(ADDRESS, "driver_app");
 
 void sendMessage(std::string top, std::string data){
     char* payload =  &data[0];
@@ -29,20 +33,26 @@ void repl_loop()
 
     bool status = true;
     int instruction;
-    std::string op, line;
+    char *line;
+    std::string op;
     std::vector<std::string> args;
 
     do {
-        std::cout << PROMPT;
-        std::getline(std::cin, line);
+        // std::cout << PROMPT;
+        // std::getline(std::cin, line);
+        line = readline(PROMPT);
 
-        if (std::cin.eof()) {
-            printf("\n");
+        if (line == NULL) {
+            free(line);
             exit(0);
-        } else if (line.length() == 0)
+        } else if (strlen(line) == 0)
             continue;
+        else
+            add_history(line);
         
         args = split_line(line);
+        free(line);
+        line = NULL;
         instruction = check_operator(args[0]);
         status = execute_instruction(instruction, args);
     } while (status);
@@ -71,19 +81,20 @@ int check_operator(std::string op)
 }
 
 
-std::vector<std::string> split_line(std::string line)
+std::vector<std::string> split_line(char *line)
 {
+    std::string in = line;
     std::vector<std::string> ret;
 
     int start = 0,
-        end = line.find(" "); // delimiter between words
+        end = in.find(" "); // delimiter between words
     
     while (end != -1) {
-        ret.push_back(line.substr(start, end - start));
+        ret.push_back(in.substr(start, end - start));
         start = end + 1;
-        end = line.find(" ", start);
+        end = in.find(" ", start);
     }
-    ret.push_back(line.substr(start, end - start));
+    ret.push_back(in.substr(start, end - start));
 
     return ret;
 }
@@ -137,7 +148,7 @@ bool execute_instruction(int instruction, std::vector<std::string> args)
             }
             win = "/car/window/" + std::to_string(window);
             sendMessage(win, "down");
-            printf("/car/window/%d down\n", window);
+            // printf("/car/window/%d down\n", window);
             break;
 
         case 3:
@@ -162,7 +173,7 @@ bool execute_instruction(int instruction, std::vector<std::string> args)
             
             win = "/car/window/" + std::to_string(window);
             sendMessage(win, "up");
-            printf("/car/window/%d up\n", window);
+            // printf("/car/window/%d up\n", window);
             break;
         
         case 4:
@@ -183,7 +194,7 @@ bool execute_instruction(int instruction, std::vector<std::string> args)
             }
 
             sendMessage(("/car/indicator/" + indicator), "on");
-            printf("/car/indicator/%s { status: 'on' }\n", indicator.c_str());
+            // printf("/car/indicator/%s { status: 'on' }\n", indicator.c_str());
             break;
 
         case 5:
@@ -204,7 +215,7 @@ bool execute_instruction(int instruction, std::vector<std::string> args)
             }
 
             sendMessage(("/car/indicator/" + indicator), "off");
-            printf("/car/indicator/%s { status: 'off' }\n", indicator.c_str());
+            // printf("/car/indicator/%s { status: 'off' }\n", indicator.c_str());
             break;
 
         case 6:
@@ -219,7 +230,7 @@ bool execute_instruction(int instruction, std::vector<std::string> args)
             }
 
             sendMessage(("/car/wipers/" + args[1]), args[2]);
-            printf("/car/wipers/%s { status: '%s' }\n", args[1].c_str(), args[2].c_str());
+            // printf("/car/wipers/%s { status: '%s' }\n", args[1].c_str(), args[2].c_str());
             break;
 
         case 7:
@@ -238,7 +249,7 @@ bool execute_instruction(int instruction, std::vector<std::string> args)
             }
 
             sendMessage(("/car/wipers/" + args[1]), wipers);
-            printf("/car/wipers/%s { status: '%s' }\n", args[1].c_str(), wipers.c_str());
+            // printf("/car/wipers/%s { status: '%s' }\n", args[1].c_str(), wipers.c_str());
             break;
 
         case 8:
@@ -248,9 +259,34 @@ bool execute_instruction(int instruction, std::vector<std::string> args)
             }
 
             sendMessage(("/car/wipers/" + args[1]), "off");
-            printf("/car/wipers/%s { status: 'off' }\n", args[1].c_str());
+            // printf("/car/wipers/%s { status: 'off' }\n", args[1].c_str());
             break;
 
+        case 9:
+            if (args[2].compare("window")) {
+                printf("Syntax error: unknown argument - %s\n", args[2].c_str());
+                return true;
+            }
+
+            if (!args[1].compare("front-left"))
+                window = 0;
+            else if (!args[1].compare("front-right"))
+                window = 1;
+            else if (!args[1].compare("back-left"))
+                window = 2;
+            else if (!args[1].compare("back-right"))
+                window = 3;
+            else {
+                printf("Syntax error: unknown argument - %s\n", args[1].c_str());
+                return true;
+            }
+
+            
+            win = "/car/window/" + std::to_string(window);
+            sendMessage(win, "stop");
+            // printf("/car/window/%d stop\n", window);
+            break;
+        
     }
 
     return true;
