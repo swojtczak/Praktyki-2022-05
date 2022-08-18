efd4c9bfd23c71ff0b23dd9e368e10f5ff787131
Piotr Zieniewicz <piti200406@gmail.com>
2022-08-18 19:10:22 +0200

    Update prompt.cpp

--- src/driver_app/prompt.cpp
+++ src/driver_app/prompt.cpp
@@ -72,15 +72,12 @@ void sendMessage(std::string top, std::string data)
 }
 
 
-<<<<<<< Updated upstream
 void my_handler(int s){
     writeOnfile("terminated by user.\n");
     exit(1);
 }
 
 
-=======
->>>>>>> Stashed changes
 void repl_loop(bool debug)
 {
     homeDir = getenv("HOME"); 
