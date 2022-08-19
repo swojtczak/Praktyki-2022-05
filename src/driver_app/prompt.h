a86d48d5fc103c20eeb8bd532125c9d326e8c70d
FilipKokowski <filipkokowski999@gmail.com>
2022-08-19 10:31:14 +0200

    Added record & stop_rec commands

--- src/driver_app/prompt.h
+++ src/driver_app/prompt.h
@@ -15,6 +15,8 @@ wipe <front or back> <on or once> \t\t-> turn on the specified wipers in single 
 wipe_w_fluid <front or back> <on or once> \t\t-> turn on the specified wipers and sprinklers in single or continous mode\n\
 wipe_off <front or back> \t\t-> turn off the specified wipers\n\
 run <file>\t\t-> run the scenario file\n\
+record <file>\t\t-> record the scenario to a file\n\
+stop_rec\t\t-> stop recording the scenario\n\
 "
 
 
@@ -36,6 +38,8 @@ const struct command command_list[] = {
     {"stop",        3},
     {"delay",       2},
     {"run",         2},
+    {"record",      2},
+    {"stop_rec",        1},
 };
 
 
@@ -43,3 +47,4 @@ void repl_loop(bool debug);
 int check_operator(std::string op);
 std::vector<std::string> split_line(char *line);
 bool execute_instruction(int instruction, std::vector<std::string> args);
+void recordAction(char* command);
