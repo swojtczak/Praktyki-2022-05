9e1c40f32f3587e2dcc66a33d3b133f20c6bb1b2
MasterSun8 <kacpi.doro@gmail.com>
2022-08-25 09:54:00 +0200

    PR5-157 GUI

--- src/driver_gui/main.cpp
+++ src/driver_gui/main.cpp
@@ -1,9 +1,70 @@
 #include <gtk/gtk.h>
- 
-static void button_clicked(GtkWidget* widget, gpointer data)
+
+static void wipe_clicked(GtkWidget* widget, gpointer data)
 {
-    g_print("Wipe is pressed\n");
+    g_print("wipe");
 }
+
+static void wipeFluid_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("wipe Fluid");
+}
+
+static void left_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("left");
+}
+
+static void right_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("right");
+}
+
+static void hazard_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("hazard");
+}
+
+static void frontLeftUp_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("frontLeftUp");
+}
+
+static void frontLeftDown_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("frontLeftDown");
+}
+
+static void frontRightUp_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("frontRightUp");
+}
+
+static void frontRightDown_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("frontRgihtDown");
+}
+
+static void backLeftUp_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("backLeftUp");
+}
+
+static void backLeftDown_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("backLeftDown");
+}
+
+static void backRightUp_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("backRightUp");
+}
+
+static void backRightDown_clicked(GtkWidget* widget, gpointer data)
+{
+    g_print("backRightDown");
+}
+
 
 GtkBuilder	*builder;
 GtkWidget	*window;
@@ -39,19 +100,19 @@ int main(int argc, char *argv[]) {
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(wipe, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(wipeFluid, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(left, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(right, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(hazard, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(frontLeftUp, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(frontLeftDown, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(frontRightUp, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(frontRightDown, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(backLeftUp, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(backLeftDown, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(backRightUp, "clicked", G_CALLBACK(button_clicked), NULL);
-    g_signal_connect(backRightDown, "clicked", G_CALLBACK(button_clicked), NULL);
+    g_signal_connect(wipe, "clicked", G_CALLBACK(wipe_clicked), NULL);
+    g_signal_connect(wipeFluid, "clicked", G_CALLBACK(wipeFluid_clicked), NULL);
+    g_signal_connect(left, "clicked", G_CALLBACK(left_clicked), NULL);
+    g_signal_connect(right, "clicked", G_CALLBACK(right_clicked), NULL);
+    g_signal_connect(hazard, "clicked", G_CALLBACK(hazard_clicked), NULL);
+    g_signal_connect(frontLeftUp, "clicked", G_CALLBACK(frontLeftUp_clicked), NULL); 
+    g_signal_connect(frontLeftDown, "clicked", G_CALLBACK(frontLeftDown_clicked), NULL);
+    g_signal_connect(frontRightUp, "clicked", G_CALLBACK(frontRightUp_clicked), NULL);
+    g_signal_connect(frontRightDown, "clicked", G_CALLBACK(frontRightDown_clicked), NULL);
+    g_signal_connect(backLeftUp, "clicked", G_CALLBACK(backLeftUp_clicked), NULL);
+    g_signal_connect(backLeftDown, "clicked", G_CALLBACK(backLeftDown_clicked), NULL);
+    g_signal_connect(backRightUp, "clicked", G_CALLBACK(backRightUp_clicked), NULL);
+    g_signal_connect(backRightDown, "clicked", G_CALLBACK(backRightDown_clicked), NULL);
 	
 	gtk_widget_show(window);
 
